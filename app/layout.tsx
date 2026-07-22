326d375b66e8730c2d3a77816d8511f265a06c75
Lucasjoaosantos <joaoluca778@gmail.com>
2026-07-22 10:52:09 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,28 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://fontana-negocios-imobiliarios.vercel.app"),
+  applicationName: "Fontana Negócios Imobiliários",
   title: {
     default: "Fontana Negócios Imobiliários",
-    template: "%s Fontana Negócios Imobiliários",
+    template: "%s | Fontana Negócios Imobiliários",
   },
   description:
     "Imóveis para negócios com facilidade. Apartamentos, casas, terrenos e mais.",
+  openGraph: {
+    siteName: "Fontana Negócios Imobiliários",
+    title: "Fontana Negócios Imobiliários",
+    description:
+      "Imóveis para negócios com facilidade. Apartamentos, casas, terrenos e mais.",
+    type: "website",
+    locale: "pt_BR",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Fontana Negócios Imobiliários",
+    description:
+      "Imóveis para negócios com facilidade. Apartamentos, casas, terrenos e mais.",
+  },
 };
 
 export default function RootLayout({
